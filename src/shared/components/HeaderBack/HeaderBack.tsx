75a1a4faa7c479f95c2749a06b7d79efee20b96f
João Fernando Zocca Calvão de Moura <127261053+JFCalvao@users.noreply.github.com>
2026-07-14 20:13:37 -0300

    Atualizei o ícone para manter o padrão de utilizar o Material Icons e SFSymbol.

--- src/shared/components/HeaderBack/HeaderBack.tsx
+++ src/shared/components/HeaderBack/HeaderBack.tsx
@@ -1,17 +1,32 @@
-import { TopBar, TopBarActions } from "@/src/features/Home/HomeHeader/styles/HomeHeader.styled";
 import { Container } from "../PageBox/PageBox.styled";
-import { ArrowLeftIcon } from "../../icons/ArrowIcon";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ThemedText } from "../ThemedText/ThemedText";
+import { IconSymbol } from "../IconSymbol/IconSymbol";
+import { TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
 
-export function HeaderBack({ title, onReturnPress }: { title: string; onReturnPress: () => void }) {
+export function HeaderBack({ title, onReturnPress }: { title: string; onReturnPress?: () => void }) {
+    const router = useRouter();
+    const onPress = onReturnPress || (() => router.replace({ pathname: "/" }));
+    
     return (
         <SafeAreaView edges={["top"]} style={{ backgroundColor: "transparent" }}>
             <Container padding={10}>
-                <TopBarActions >
-                    <ArrowLeftIcon size={32} color="#0C5347" onPress={onReturnPress} />
+                <Container flexDirection="row" centerHorizontal gap={10}>
+                    <TouchableOpacity
+                        onPress={onPress}
+                        style={{
+                            backgroundColor: "#E4F3EA",
+                            borderRadius: 10,
+                            padding: 5,
+                            alignItems: "center",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <IconSymbol size={28} name="arrow.left" color="#0C5347" />
+                    </TouchableOpacity>
                     <ThemedText type="pagetitle">{title}</ThemedText>
-                </TopBarActions>
+                </Container>
             </Container>
         </SafeAreaView>
     );
--- src/shared/components/IconSymbol/IconSymbol.tsx
+++ src/shared/components/IconSymbol/IconSymbol.tsx
@@ -14,6 +14,7 @@ type IconSymbolName = keyof typeof MAPPING;
  * - see SF Symbols in the [SF Symbols](https://developer.apple.com/sf-symbols/) app.
  */
 const MAPPING = {
+  "arrow.left": "arrow-back",
   "house.fill": "home",
   "paperplane.fill": "send",
   "chevron.left.forwardslash.chevron.right": "code",
